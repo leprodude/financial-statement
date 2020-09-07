8df52bd3c637bb9563748dffac54a0906a124375
LeProdude <47125897+leprodude@users.noreply.github.com>
2020-09-07 02:01:26 +0200

    clean up outcommented elements

--- src/FinancialStatement.tsx
+++ src/FinancialStatement.tsx
@@ -18,19 +18,9 @@ const FinancialStatement: React.FC = (props) => {
     <>
       <Section className="FinancialStatement-hero has-background-white-ter hero is-fullheight-with-navbar">
         <Columns className="is-multiline FinancialStatementColumns" breakpoint="tablet">
-
           <InOutBox financialType={FinancialType.INCOME} size={6}></InOutBox>
-          {/* <Columns.Column><h2>Column</h2></Columns.Column>
-          <Columns.Column><h2>Column</h2></Columns.Column>
-          <Columns.Column><h2>Column</h2></Columns.Column>
-          <Columns.Column><h2>Column</h2></Columns.Column> */}
-
           <InOutBox financialType={FinancialType.EXPENSE} size={6}></InOutBox>
-          {/* <Columns.Column size="full"></Columns.Column> */}
-
           <InOutBox financialType={FinancialType.ASSET} size={6}></InOutBox>
-          {/* <Columns.Column size="1"></Columns.Column> */}
-
           <InOutBox financialType={FinancialType.LIABILITY} size={6}></InOutBox>
         </Columns>
       </Section>
